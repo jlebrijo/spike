24994783714cd2399dbbf9afd729a342fcb3ec46
Juan Lebrijo <juan@lebrijo.com>
2019-10-22 10:01:26 +0200

    fix bootstrap.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,7 +15,7 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
-import "controllers"
+import JQuery from 'jquery';
+window.$ = window.JQuery = JQuery;
 import 'bootstrap'
-import '../stylesheets/application'
+import 'controllers'
