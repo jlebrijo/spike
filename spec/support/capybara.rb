61d9561fcc8afffaa87a9078788b531713594826
Juan Lebrijo <juan@lebrijo.com>
2019-11-03 13:02:52 +0100

    Fix tests

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -7,6 +7,7 @@ Capybara.register_driver :selenium_chrome_headless do |app|
   options.add_argument("--window-size=1024,768")
   options.add_argument("--headless")
   options.add_argument("--disable-gpu")
+  options.add_argument('--no-sandbox')
 
   Capybara::Selenium::Driver.new app,
                                  browser: :chrome,
